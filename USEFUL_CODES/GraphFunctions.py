4aabdfdcf1334ab53c673b94736e6615139810f9
fadi alahmad <fadi.alahmad.alomar@gmail.com>
2023-06-01 19:21:17 +0300

    Created GraphFunctions file and added Topological Sort function

---
+++ USEFUL_CODES/GraphFunctions.py
@@ -0,0 +1,30 @@
+from typing import List, DefaultDict, Set, Dict
+from collections import defaultdict, deque
+
+
+def topological_sort(nodes: Set, dep: Dict[int, Set[int]] | DefaultDict[int, Set[int]]) -> List[int]:
+    """
+    Topological sort using number of in bound edges order
+
+    :param nodes: Set of the nodes to be sorted
+    :param dep: dependency hashmap
+    :return: Sorted list of the given nodes
+    """
+    in_ord: DefaultDict[int, int] = defaultdict(int)
+    for li in nodes:
+        if li in dep:
+            for lj in dep[li]:
+                in_ord[lj] += 1
+    ans: List[int] = []
+    d = deque()
+    for li in nodes:
+        if not in_ord[li]:
+            d.append(li)
+    while d:
+        ans.append(d.popleft())
+        for li in dep[ans[-1]]:
+            in_ord[li] -= 1
+            if not in_ord[li]:
+                d.append(li)
+
+    return ans
